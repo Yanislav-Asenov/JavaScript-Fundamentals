3180043b47a01c7cfc7205d7e94b73c9a3d2cbd2
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-10-01 17:36:45 +0300

    10/10 all exercises done

--- 09.Array and Matrices Exercises/10.orbit.js
+++ 09.Array and Matrices Exercises/10.orbit.js
@@ -3,7 +3,7 @@ function solve (arr) {
   let rows = matrixDimensions[0]
   let cols = matrixDimensions[1]
   let startCoordinates = arr[1].split(/\s+/).filter(x => x !== '').map(Number)
-  let startRow = startCoordinates[0]
+  let starRow = startCoordinates[0]
   let starCol = startCoordinates[1]
 
   let matrix = []
@@ -15,10 +15,51 @@ function solve (arr) {
     matrix.push(currentRow)
   }
 
-  matrix[startRow][starCol] = 1
+  matrix[starRow][starCol] = 1
+
+  let cellCounter = 1
+  let num = 2
+  let startCol = starCol - 1
+  let startRow = starRow - 1
+  let endRow = starRow + 1
+  let endCol = starCol + 1
+
+  while (cellCounter < rows * cols) {
+    for (let row = startRow; row <= endRow; row++) {
+      for (let col = startCol; col <= endCol; col++) {
+        if (row >= 0 && row < rows) {
+          if (col >= 0 && col < cols) {
+            if (!matrix[row][col]) {
+              matrix[row][col] = num
+              cellCounter++
+            }
+          }
+        }
+      }
+    }
+    num++
+    startCol--
+    startRow--
+    endCol++
+    endRow++
+  }
+
+  for (let row = 0; row < rows; row++) {
+    console.log(matrix[row].join(' '))
+  }
 }
 
-solve([
-  '5 5',
-  '2 2'
-])
+// solve([
+//   '5 5',
+//   '2 2'
+// ])
+
+// solve([
+//   '4 4',
+//   '0 0'
+// ])
+
+// solve([
+//   '3 3',
+//   '2 2'
+// ])
